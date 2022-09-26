d7966d0ba36551d278967d3d976bf722cba261c5
user2020-py <x_bomba@inbox.ru>
2022-09-26 23:25:32 +0500

    first commit

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,13 +1,18 @@
 import { Carousel } from 'antd';
 import { useRef } from 'react';
-import { Container, Arrow } from "./style.js"
+import { Container, Arrow, Content, DarkLayer, Bed, Bath, Car, Area } from "./style.js"
+import { data } from '../../mock/data.js';
+import { Button } from '../Generic'
 
 const contentStyle = {
   height: '570px',
   color: '#fff',
-  lineHeight: '160px',
   textAlign: 'center',
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   background: '#364d79',
+  position: "relative"
 };
 
 const GenCarousel = () => {
@@ -20,18 +25,44 @@ const GenCarousel = () => {
     return (
         <Container>
             <Carousel autoplay ref={slider}>
-                <div>
-                    <h3 style={contentStyle}>Hello</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>I'm Carousel</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>Move me</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>It's Enjoy!</h3>
-                </div>
+                {
+                    data?.map(({id, title, subtitle, settings, pricePerMonth, img}) => {
+                        return (
+                            <div key={id}>
+                                <div style={contentStyle}>
+                                    <img src={img} alt={title} className="productImg"/>
+                                    <DarkLayer/>
+                                    <Content>
+                                        <div className='title'>{title}</div>
+                                        <div className='subtitle'>{subtitle}</div>
+                                        <div className='settings'>
+                                            <div>
+                                                <Bed/>
+                                                <span>{settings.beds} Beds</span>
+                                            </div>
+                                            <div>
+                                                <Bath/>
+                                                <span>{settings.baths} Bath</span>
+                                            </div>
+                                            <div>
+                                                <Car/>
+                                                <span>{settings.garage} Garages</span>
+                                            </div>
+                                            <div>
+                                                <Area style={{width: "19px", height: "19px"}}/>
+                                                <span>{settings.area} Sq Ft</span>
+                                            </div>
+                                        </div>
+                                        <div className='price'>${pricePerMonth}/mo</div>
+                                        <div className='readmore'>
+                                            <Button type={"dark"}>Read more</Button>
+                                        </div>
+                                    </Content>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </Carousel>
             <div className="arrow">
                 <div data-name="prev" onClick={onMove}>
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import { ReactComponent as arrowIcon } from "../../assets/icons/arrow.svg";
+import { ReactComponent as bed } from "../../assets/icons/bed.svg";
+import { ReactComponent as bath } from "../../assets/icons/bath.svg";
+import { ReactComponent as car } from "../../assets/icons/car.svg";
+import { ReactComponent as area } from "../../assets/icons/area.svg";
 
 export const Container = styled.div`
     position: relative;
@@ -24,8 +28,61 @@ export const Container = styled.div`
             cursor: pointer;
         }
     }
+    .productImg{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 `
 
+export const Content = styled.div`
+    position: relative;
+    z-index: 3;
+    >*{
+        color: #fff;
+    }
+    .title{
+        font-size: 44px;
+        font-weight: 700;
+    }
+    .subtitle{
+        font-size: 16px;
+        font-weight: 400;
+    }
+    .settings{
+        margin: 24px 0;
+        display: inline-flex;
+        column-gap: 24px;
+        >div{
+            display: inline-flex;
+            flex-direction: column;
+            align-items: center;
+            row-gap: 6.7px;
+        }
+    }
+    .price{
+        font-size: 28px;
+        font-weight: 600;
+        margin-bottom: 34px;
+    }
+    .readmore>button{
+        height: 50px;
+        width: 178px;
+    }
+`
+
+export const DarkLayer = styled.div`
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.65);
+`
+export const Bed = styled(bed)``
+export const Bath = styled(bath)``
+export const Car = styled(car)``
+export const Area = styled(area)``
 export const Arrow = styled(arrowIcon)`
     transform: ${({right}) => right && "rotateY(-180deg)"}
 `
