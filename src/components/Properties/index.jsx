541eab05cc3e78a950162984f3bdb7df0fb535c1
user2020-py <x_bomba@inbox.ru>
2022-10-01 22:09:17 +0500

    fixed bugs

--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -2,7 +2,7 @@ import Card from "../Card";
 import { useContext } from "react";
 import { ContextAPI } from "../../context";
 import Filter from "../Filter"
-import { Houses } from "./style";
+import { Houses, Head } from "./style";
 
 const Properties = () => {
     const { housesList } = useContext(ContextAPI)
@@ -10,21 +10,35 @@ const Properties = () => {
     return (
         <div>
             <Filter/>
-            <Houses>
-                {
-                    housesList ? housesList.data?.map((house, i) => 
-                        <Card
-                            key={i}
-                            title={house.name}
-                            subtitle={`${house.address}, ${house.country}`}
-                            settings={house.houseDetails}
-                            houseImg={house.attachments[0].imgPath}
-                            afterPrice={house.price}
-                            pricePerMonth={house.salePrice}
-                        />
-                    ) : <h1>Loading...</h1>
-                }
-            </Houses>
+            <div className='container'>
+                <Head>
+                    <h1>Properties</h1>
+                    <p>Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.</p>
+                    <div>
+                        <span>
+                            <b>{housesList.map.size}</b> results
+                        </span>
+                        <select name="sort" id="sort" defaultValue={"all"}>
+                            <option value="all">All houses</option>
+                        </select>
+                    </div>
+                </Head>
+                <Houses>
+                    {
+                        housesList ? housesList.data?.map((house, i) => 
+                            <Card
+                                key={i}
+                                title={house.name}
+                                subtitle={`${house.address}, ${house.country}`}
+                                settings={house.houseDetails}
+                                houseImg={house.attachments[0].imgPath}
+                                afterPrice={house.price}
+                                pricePerMonth={house.salePrice}
+                            />
+                        ) : <h1>Loading...</h1>
+                    }
+                </Houses>
+            </div>
         </div>
     )
 }
--- src/components/Properties/style.js
+++ src/components/Properties/style.js
@@ -5,3 +5,21 @@ export const Houses = styled.div`
     flex-wrap: wrap;
     justify-content: space-around;
 `
+
+export const Head = styled.div`
+    margin-top: 60px;
+    text-align: center;
+    >div{
+        display: flex;
+        justify-content: space-between;
+        margin: 70px 0 16px 0;
+        p{
+            margin: 0;
+            font-weight: 400;
+        }
+        h1{
+            margin: 0 0 8px 0;
+            font-weight: 700;
+        }
+    }
+`
